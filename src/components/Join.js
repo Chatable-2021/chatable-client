cd4d56c0ada2ced040851b64710a1846f494ef07
davidabutler92 <davidabutler92@yahoo.com>
2021-01-15 13:37:07 -0800

    add useState hooks in Join component

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Join() {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  
   return (
-    <div>
-      Join
-    </div>
+    <h1>Join</h1>
   );
 }
