7b18c459b20e1eb8bfe1010a258ea86fbaedcc4a
davidabutler92 <davidabutler92@yahoo.com>
2021-01-16 14:24:36 -0800

    add sign up styles

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { signupSchema } from './Signup.schema';
+import styles from './Signup.css';
 
-const onSubmit = (data) => {
-  console.log(data);
-};
+
 
 export default function Signup() {
   const { register, handleSubmit, watch, errors } = useForm({
@@ -15,25 +15,51 @@ export default function Signup() {
     defaultValues: { email: '', password: '', name: '' },
   });
 
+  const showNameError = Boolean(errors.name);
+  const showEmailError = Boolean(errors.email);
+  const showPasswordError = Boolean(errors.password);
+  const showErrorOrEmptyString = (shouldShow, message) =>
+    shouldShow ? message : '';
+
+  const history = useHistory();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    history.push('/join');
+  };
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h1>Sign Up</h1>
-      <input 
+    <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
+      <h1 className={styles.heading}>Sign Up</h1>
+      <input
+        className={styles['form-input']}
         name='name'
         ref={register}
-        placeholder='User Name'>
-      </input>
-      <input 
-        name='email' 
-        ref={register} 
-        placeholder='Email' />
+        placeholder='User Name'
+      />
+      <p className={styles.errorsMessage}>
+        {showErrorOrEmptyString(showNameError, errors.name?.message)}
+      </p>
       <input
+        className={styles['form-input']}
+        name='email'
+        ref={register}
+        placeholder='Email'
+      />
+      <p className={styles.errorsMessage}>
+        {showErrorOrEmptyString(showEmailError, errors.email?.message)}
+      </p>
+      <input
+        className={styles['form-input']}
         name='password'
         ref={register}
         type='password'
         placeholder='Password'
-      ></input>
-      <button type='submit'>Sign Up</button>
+      />
+      <p className={styles.errorsMessage}>
+        {showErrorOrEmptyString(showPasswordError, errors.password?.message)}
+      </p>
+      <button className={styles.submitButton} type='submit'>Sign Up</button>
     </form>
   );
 }
