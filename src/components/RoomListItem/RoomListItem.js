9f5c72329098471ff4431db8f1fd836ab9496158
Zach Butler <zach.standley93@gmail.com>
2021-02-21 15:34:36 -0800

    add change to ListItem, error caused by passing in custom compoennt

--- src/components/RoomListItem/RoomListItem.js
+++ src/components/RoomListItem/RoomListItem.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './RoomListItem.css';
+import { Link as RouterLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
-
 const RoomListItem = ({ room }) => {
-  const CustomLink = (props) => <Link to={`/room/?id=${room.id}`} {...props} />;
-
   return (
-    <ListItem
-      primary={room?.name}
-      component={CustomLink}
-      button
-    >
+    <ListItem to={`/room/?id=${room.id}`} component={RouterLink} button divider>
       <ListItemText primary={room?.name} />
     </ListItem>
   );
