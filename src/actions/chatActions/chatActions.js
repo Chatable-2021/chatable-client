de4be30918865a9eb96b757eea448fbd9d64860c
Zach Butler <zach.standley93@gmail.com>
2021-02-21 17:53:47 -0800

    add addMessage action creator and corresponding test

--- src/actions/chatActions/chatActions.js
+++ src/actions/chatActions/chatActions.js
@@ -6,3 +6,9 @@ export const setMessages = (roomId, messages) => ({
     messages,
   },
 });
+
+export const ADD_MESSAGE = 'ADD_MESSAGE';
+export const addMessage = message => ({
+  type: ADD_MESSAGE,
+  payload: message,
+});
--- src/actions/chatActions/chatActions.test.js
+++ src/actions/chatActions/chatActions.test.js
@@ -1,19 +1,30 @@
-import { setMessages, SET_MESSAGE } from './chatActions';
+import {
+  addMessage,
+  ADD_MESSAGE,
+  setMessages,
+  SET_MESSAGE,
+} from './chatActions';
 
 describe('chat actions', () => {
-  it('sets messages with setMessages action creator', () => {
-    const arr = [
+  it('creates a set message action with the setMessages action creator', () => {
+    const messages = [
       {
-        id: 1,
-        text: 'hello',
+        id: '1',
+        chatRoomId: '1',
+        userId: '1',
+        userName: 'Zach Butler',
+        messageText: 'new message',
       },
       {
-        id: 2,
-        text: 'hello',
+        id: '2',
+        chatRoomId: '1',
+        userId: '2',
+        userName: 'David Butler',
+        messageText: 'another message',
       },
     ];
 
-    const action = setMessages(1, arr);
+    const action = setMessages(1, messages);
 
     expect(action).toEqual({
       type: SET_MESSAGE,
@@ -21,14 +32,43 @@ describe('chat actions', () => {
         roomId: 1,
         messages: [
           {
-            id: 1,
-            text: 'hello',
+            id: '1',
+            chatRoomId: '1',
+            userId: '1',
+            userName: 'Zach Butler',
+            messageText: 'new message',
           },
           {
-            id: 2,
-            text: 'hello',
+            id: '2',
+            chatRoomId: '1',
+            userId: '2',
+            userName: 'David Butler',
+            messageText: 'another message',
           },
         ],
+      },
+    });
+  });
+
+  it('creates a add message action with the addMessage action creator', () => {
+    const message = {
+      id: '1',
+      chatRoomId: '2',
+      userId: '3',
+      userName: 'Zach Butler',
+      messageText: 'new message',
+    };
+
+    const action = addMessage(message);
+
+    expect(action).toEqual({
+      type: ADD_MESSAGE,
+      payload: {
+        id: '1',
+        chatRoomId: '2',
+        userId: '3',
+        userName: 'Zach Butler',
+        messageText: 'new message',
       },
     });
   });
