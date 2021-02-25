561fdc7dae6c241ab547fdc240ea6c9b3d471390
Zach Butler <zach.standley93@gmail.com>
2021-02-25 14:10:19 -0800

    add signup styles js

---
+++ src/components/Signup/Signup.styles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    width: 400,
+    padding: 15,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default useStyles;
