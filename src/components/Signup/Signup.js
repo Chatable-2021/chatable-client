b9746600e7be19f45421278395ed244c2dd68cc5
Zach Butler <zach.standley93@gmail.com>
2021-02-24 18:00:03 -0800

    started the refactor on signup component

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -5,15 +5,30 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { signupSchema } from './Signup.schema';
 import styles from './Signup.css';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { AppInputWithError, AppButtonWithError } from '../controls';
+
+const useStyles = makeStyles({
+  root: {
+    width: 400,
+    padding: 15,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 function Signup({ socket, setUser }) {
+  const classes = useStyles();
   const [error, setError] = useState('');
   const [invalid, setInvalid] = useState(false);
 
   const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(signupSchema),
     mode: 'onBlur',
-    reValidateMode: 'onBlur'
+    reValidateMode: 'onBlur',
   });
 
   const showNameError = Boolean(errors.name);
@@ -24,64 +39,61 @@ function Signup({ socket, setUser }) {
 
   const history = useHistory();
 
-  useEffect(() => {
-    socket.on('AUTH_RESULTS', (authResults) => {
-      if(!authResults.success) {
-        setError(authResults.message);
-        setInvalid(true);
-      } else {
-        setUser(authResults.user);
-        setInvalid(false);
-        history.push('/room');
-      }
-      return () => socket.off('AUTH_RESULTS');
-    });
-  }, []);
+  // useEffect(() => {
+  //   socket.on('AUTH_RESULTS', (authResults) => {
+  //     if(!authResults.success) {
+  //       setError(authResults.message);
+  //       setInvalid(true);
+  //     } else {
+  //       setUser(authResults.user);
+  //       setInvalid(false);
+  //       history.push('/room');
+  //     }
+  //     return () => socket.off('AUTH_RESULTS');
+  //   });
+  // }, []);
 
   const handleSignup = formValues => {
     socket.emit('SIGN_UP', formValues);
   };
 
+  // work on styles object for the first input
   return (
     <>
-      <form className={styles.container} onSubmit={handleSubmit(handleSignup)}>
-        <h1 className={styles.heading}>Sign Up</h1>
-        <input
-          className={styles.formInput}
-          name="name"
-          ref={register}
-          placeholder="User Name"
+      <form className={classes.root} onSubmit={handleSubmit(handleSignup)}>
+        <AppInputWithError
+          errors={errors}
+          autoFocus={true}
+          emailOrPasswordOrName='name'
+          register={register}
+          styles={styles}
         />
         <p className={styles.errorsMessage}>
           {showErrorOrEmptyString(showNameError, errors.name?.message)}
         </p>
         <input
           className={styles.formInput}
-          name="email"
+          name='email'
           ref={register}
-          placeholder="Email"
+          placeholder='Email'
         />
         <p className={styles.errorsMessage}>
           {showErrorOrEmptyString(showEmailError, errors.email?.message)}
         </p>
         <input
           className={styles.formInput}
-          name="password"
+          name='password'
           ref={register}
-          type="password"
-          placeholder="Password"
+          type='password'
+          placeholder='Password'
         />
         <p className={styles.errorsMessage}>
           {showErrorOrEmptyString(showPasswordError, errors.password?.message)}
         </p>
-        <button className={styles.submitButton}  type="submit">
+        <button className={styles.submitButton} type='submit'>
           Sign Up
         </button>
-        <p className={styles.errorsMessage}> 
-          { invalid
-            ? error
-            : '' }
-        </p>
+        <p className={styles.errorsMessage}>{invalid ? error : ''}</p>
       </form>
     </>
   );
@@ -92,9 +104,9 @@ Signup.propTypes = {
   socket: PropTypes.shape({
     emit: PropTypes.func.isRequired,
     on: PropTypes.func.isRequired,
-    off: PropTypes.func.isRequired
+    off: PropTypes.func.isRequired,
   }),
-  setUser: PropTypes.func.isRequired
+  setUser: PropTypes.func.isRequired,
 };
 
 export default Signup;
