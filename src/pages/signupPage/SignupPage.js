3625a3e108106fcc920ceee9016f0979507ecb01
Zach Butler <zach.standley93@gmail.com>
2021-02-25 14:11:36 -0800

    add signup page and corresponding styles

--- src/pages/signupPage/SignupPage.js
+++ src/pages/signupPage/SignupPage.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Container, Box } from '@material-ui/core';
 
+import AppText from '../../components/appText/AppText';
+import blueLogo from '../../assets/blueLogo.png';
 import Signup from '../../components/Signup/Signup';
+import useStyles from './Signup.styles';
 
 function SignupPage({ socket, setUser }) {
+  const classes = useStyles();
+
   return (
-    <>
-      <Signup socket={socket} setUser={setUser} />
-    </>
+    <Container
+      disableGutters={true}
+      className={classes.root}
+      component='section'
+    >
+      <Box className={classes.contentContainer}>
+        <Box className={classes.logoContainer}>
+          <img src={blueLogo} className={classes.logo} />
+        </Box>
+        <AppText variant='h4' component='h1' className={classes.title}>
+          Create your account
+        </AppText>
+        <Signup
+          socket={socket}
+          setUser={setUser}
+          styles={{
+            inputWidth: '100%',
+            buttonWidth: '100%',
+            buttonVariant: 'contained',
+          }}
+        />
+      </Box>
+    </Container>
   );
 }
 
-SignupPage.propTypes = {};
+SignupPage.propTypes = {
+  socket: PropTypes.shape({
+    emit: PropTypes.func.isRequired,
+    on: PropTypes.func.isRequired,
+    close: PropTypes.func.isRequired,
+  }),
+  setUser: PropTypes.func.isRequired,
+};
 
 export default SignupPage;
