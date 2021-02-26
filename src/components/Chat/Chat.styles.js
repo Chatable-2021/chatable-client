30aef198262ba3715e8b2eac6f0cd0920f40904a
Zach Butler <zach.standley93@gmail.com>
2021-02-25 22:01:34 -0800

    add styles

---
+++ src/components/Chat/Chat.styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    height: '100%',
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+});
+
+export default useStyles;
