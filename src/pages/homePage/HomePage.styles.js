03b553e47201206e9a0b517b7954e9bbd68201b6
Zach Butler <zach.standley93@gmail.com>
2021-02-25 22:04:01 -0800

    changed background color

--- src/pages/homePage/HomePage.styles.js
+++ src/pages/homePage/HomePage.styles.js
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   root: {
+    backgroundColor: theme.palette.background.default,
     display: 'flex',
     minHeight: '100vh',
     maxHeight: '100vh',
--- src/pages/loginPage/LoginPage.styles.js
+++ src/pages/loginPage/LoginPage.styles.js
@@ -1,7 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
+    backgroundColor: theme.palette.background.default,
     minHeight: '100vh',
     maxHeight: '100vh',
     width: '100%',
@@ -35,6 +36,6 @@ const useStyles = makeStyles({
     fontWeight: 900,
     margin: '32px 0 12px 15px',
   },
-});
+}));
 
 export default useStyles;
